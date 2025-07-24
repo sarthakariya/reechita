850eed104e62487135081b9763ac09b7b3ee2850
S@rth@k ;) <26sarthakbasu@gmail.com>
2025-07-24 10:16:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-script.js
-
 document.addEventListener('DOMContentLoaded', () => {
     const mainContainer = document.querySelector('.container');
     const backgroundElementsContainer = document.querySelector('.background-elements');
